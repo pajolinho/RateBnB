2d3d0785faaf6b603142a2b016ec0467de8a72cd
meal49 <s_alkas24@stud.hwr-berlin.de>
2025-07-02 15:40:18 +0200

    test

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 import random
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-#prototype
-bnbs = [
-    {"id": 1, "title": "Berlin Studio", "city": "Berlin", "price": 120},
-    {"id": 2, "title": "Warschau Flat", "city": "Warschau", "price": 90}
-]
+# #prototype
+# bnbs = [
+#     {"id": 1, "title": "Berlin Studio", "city": "Berlin", "price": 120},
+#     {"id": 2, "title": "Warschau Flat", "city": "Warschau", "price": 90}
+# ]
 
 app = Flask(__name__)
+# DAtenbank Konfi
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+#Table erstellen
+class Bnb (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String(150), nullable=False)
+    title = db.Column(db.String(600), nullable=False)
+    location = db.Column(db.String(150), nullable=False)
+    beds = db.Column(db.Integer, nullable=False)
+    rooms = db.Column(db.Integer, nullable=True)
+    price_per_night = db.Column(db.Integer, nullable=False)
+    link = db.Column(db.String, nullable=False)
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -29,5 +46,9 @@ def index():
             expected = "twoExpensive"
 
         return render_template("index.html", score=score, listing1=bnb1, listing2=bnb2, expected=expected)
+
 if __name__ == "__main__":
+    #Datenbank speichern falls im app context ausgeführt wird
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
